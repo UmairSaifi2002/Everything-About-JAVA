ff54a7052ea02607287d03ae2d648b50437a3dd3
Umair Saifi <umairsaifi2002@gmail.com>
2025-07-06 15:11:00 +0530

    Create IO3.java

---
+++ 01_Basics/IO3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class IO3{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("What is yout name:");
+    String name = sc.next();
+    System.out.println("What is your age:");
+    int age = Integer.parseInt(sc.next());
+  }
+}
